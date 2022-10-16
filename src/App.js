5218c52182278530e37016180ce0fa0db1e71dcf
Tiffani Burk <greengurl1088@yahoo.com>
2022-10-16 16:03:29 -0500

    added function for shuffling the cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+//create an array of the images 
+const cardImages = [
+  {'src' : 'images/cauldrin.png'},
+  {'src' : 'images/bat.png'},
+  {'src' : 'images/ghost.png'},
+  {'src' : 'images/owl.png'},
+  {'src' : 'images/poison.png'},
+  {'src' : 'images/pumpkin.png'} 
+]
+
+
+
 function App() {
+//useState
+const [cards, setCards] = useState([])
+const [turns, setTurns] = useState(0)
+
+//shuffle cards
+//create function to duplicate the cardImages array and add to new array
+
+const shuffleCards = () => {
+  const shuffledCards = [...cardImages, ...cardImages] //spread operator copies the array 
+  .sort(() => Math.random() - 0.5)
+  .map((card) => ({ ...card, id: Math.random()}))
+
+  setCards(shuffledCards)
+  setTurns(0)
+}
+
+console.log(cards, turns)
+
   return (
-  <div>
+  <div className='App'>
     <h2> Magic Memory</h2>
-    <h6>A game to test your memory...</h6>
+    <h6 className='subTitle'>A game to test your memory...</h6>
+    <button onClick={shuffleCards}>New Game</button>
+  
   </div>
   );
 }
