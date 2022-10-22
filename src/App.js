517577fd57cc612984dfa6f1e1305ec6b6c58de3
Tiffani Burk <greengurl1088@yahoo.com>
2022-10-21 19:32:10 -0500

    click event added

--- src/App.js
+++ src/App.js
@@ -4,56 +4,66 @@ import { SingleCard } from './components/SingleCard';
 
 //create an array of the images 
 const cardImages = [
-  {'src' : 'img/cauldron.png'},
-  {'src' : 'img/bat.png'},
-  {'src' : 'img/ghost.png'},
-  {'src' : 'img/owl.png'},
-  {'src' : 'img/poison.png'},
-  {'src' : 'img/pumpkin.png'} 
+  { 'src': 'img/cauldron.png' },
+  { 'src': 'img/bat.png' },
+  { 'src': 'img/ghost.png' },
+  { 'src': 'img/owl.png' },
+  { 'src': 'img/poison.png' },
+  { 'src': 'img/pumpkin.png' }
 ]
 
 
 
 function App() {
-//useState
-const [cards, setCards] = useState([])
-const [turns, setTurns] = useState(0)
+  //useState
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
-//shuffle cards
-//create function to duplicate the cardImages array and add to new array
-const shuffleCards = () => {
-  const shuffledCards = [...cardImages, ...cardImages] //spread operator copies the array 
-  .sort(() => Math.random() - 0.5)
-  .map((card) => ({ ...card, id: Math.random()}))
+  //shuffle cards
+  //create function to duplicate the cardImages array and add to new array
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages] //spread operator copies the array 
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
 
-  setCards(shuffledCards)
-  setTurns(0)
-}
-
-console.log(cards, turns)
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+//this function will take the card the user has chosen as an argument
+// Pass this function as a prop to SingleCard
+  const handleChoice = (card) => {
+console.log(card)
+  }
 
   return (
-  <div className='App'>
-    <h2> Magic Memory</h2>
-    <h6 className='subTitle'>A game to test your memory...</h6>
-    <button onClick={shuffleCards}>New Game</button>
+    <div className='App'>
+      <h2> Magic Memory</h2>
+      <h6 className='subTitle'>A game to test your memory...</h6>
+      <button onClick={shuffleCards}>New Game</button>
 
-<div className='game-center'>
-    <div className='card-grid'>
-      {cards.map(card => (
-        <div className='card' key={card.id}>
-           <SingleCard key={card.id} card={card} /> 
+      <div className='game-center'>
+        <div className='card-grid'>
+          {cards.map(card => (
+            <div className='card' key={card.id}>
+              <SingleCard 
+              key={card.id} 
+              card={card}
+              handleChoice={handleChoice}
+              />
+            </div>
+
+
+          ))}
         </div>
-       
 
-      ))}
+      </div>
     </div>
-  
-  </div>
-  </div>
   );
 }
 
 export default App;
 
 // We send the {card} over to SingleCard.js in a prop
+// The user has two select two cards, so create a useState to handle the states of those choices
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,15 +1,23 @@
 import './SingleCard.css'
 
-export const SingleCard = ({ card }) => {
+export const SingleCard = ({ card, handleChoice }) => {
+
+    // this function will update the state of the choiceOne or choiceTwo,
+    // however, this function cant do that because the states exist in App.js, so create a functio in there, pass it as a prop and and call it in here. 
+    const handleClick = () => {
+        handleChoice(card)
+    }
     return (
         <div className='card'>
             <div>
             {/* create a dynamic image for the front of card */}
             <img className='front' src={card.src} alt='card front' /> 
-            <img className='back' src='img/CardBackground.png' alt='card back' />
+            <img className='back' src='img/CardBackground.png' alt='card back' onClick={handleClick} />
             </div>
     </div>
     )
 }
 
 // to access card.src, we have to destructure the card on App.js and send a prop over to this file
+// create an onClick on the back of the card and set it to a function
+// create a function called handleClick insdie the SingleCard component 
